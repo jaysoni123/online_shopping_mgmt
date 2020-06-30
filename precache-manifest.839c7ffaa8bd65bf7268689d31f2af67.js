0518dd4f08d41c7a7e05c75eb06396c0de64ad6e
Dhananjay Choksi <soni.jay008@gmail.com>
2020-06-30 14:13:32 +0530

    Updates

---
+++ precache-manifest.839c7ffaa8bd65bf7268689d31f2af67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4a67df33f156523959264b3a005a1f",
+    "url": "/online_shopping_mgmt/index.html"
+  },
+  {
+    "revision": "453f810f59461db46fef",
+    "url": "/online_shopping_mgmt/static/css/main.22678f59.chunk.css"
+  },
+  {
+    "revision": "ae9481126844a002b4b5",
+    "url": "/online_shopping_mgmt/static/js/2.20e71985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online_shopping_mgmt/static/js/2.20e71985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453f810f59461db46fef",
+    "url": "/online_shopping_mgmt/static/js/main.5a7836a4.chunk.js"
+  },
+  {
+    "revision": "89c71ac9836dd8d78abc",
+    "url": "/online_shopping_mgmt/static/js/runtime-main.63d303db.js"
+  }
+]);
